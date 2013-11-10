45000f1b7976b2604cb112cea41072d262b7f0cf
David N. Robinson <oaguy1@gmail.com>
2013-11-09 23:02:37 -0500

    please, please work

--- TagML/main.py
+++ TagML/main.py
@@ -3,6 +3,7 @@ import worldmap.worldmap as wm
 def __init__():
     world_map = wm.WorldMap(5,3)
     world_map.test_fill()
-    print world_map
+    print world_ma
+    print 'Thank you'
 
 __init__()
