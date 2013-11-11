89af7cc9f9284a633412b785a1c12bb642dee098
David N. Robinson <oaguy1@gmail.com>
2013-11-10 19:22:54 -0500

    Fixed small error in main.py where world_map was not properly identified

--- TagML/main.py
+++ TagML/main.py
@@ -3,7 +3,7 @@ import worldmap.worldmap as wm
 def __init__():
     world_map = wm.WorldMap(5,3)
     world_map.test_fill()
-    print world_ma
+    print world_map
     print 'Thank you'
 
 __init__()
